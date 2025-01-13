268cc1124621162c5ddbff13f572c8062356b7c9
Pe3et <wolfpeeet@googlemail.com>
2025-01-13 12:48:11 +0100

    added basic ModelSerializers

---
+++ Backend Join/join/api/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+
+from join.models import Contact, Subtask, Task
+
+
+class ContactSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Contact
+        fields = '__all__'
+
+
+class TaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Task
+        fields = '__all__'
+
+
+class SubtaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Subtask
+        fields = '__all__'
