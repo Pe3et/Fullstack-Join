46dc03f4f7b7015a3b94b19b7bce735bda0b0ed3
Pe3et <wolfpeeet@googlemail.com>
2025-01-16 14:29:32 +0100

    subtask deletion works

--- Backend Join/join/api/views.py
+++ Backend Join/join/api/views.py
@@ -1,3 +1,4 @@
+from typing import Type
 from rest_framework import status, viewsets
 from rest_framework.response import Response
 
@@ -48,16 +49,28 @@ class TaskViewSet(viewsets.ModelViewSet):
         serializer.is_valid(raise_exception=True)
         self.perform_update(serializer)
 
-        subtasks_data = request.data.get('subtasks', [])
-        for subtask_data in subtasks_data:
-            if 'id' not in subtask_data:
-                subtask_data['task'] = instance.id
-                subtask_serializer = SubtaskSerializer(data=subtask_data)
+        new_subtasks_data = request.data.get('subtasks', [])
+
+        # check for subtasks to delete
+        existing_subtasks_queryset = instance.subtasks.all()
+        new_subtasks_ids = []
+        for subtask_data in new_subtasks_data:
+            if 'id' in subtask_data:
+                new_subtasks_ids.append(subtask_data['id'])
+        for existing_subtask in existing_subtasks_queryset:
+            if existing_subtask.id not in new_subtasks_ids:
+                existing_subtask.delete()
+                
+        # add new subtasks or edit existing subtasks
+        for new_subtask_data in new_subtasks_data:
+            if 'id' not in new_subtask_data:
+                new_subtask_data['task'] = instance.id
+                subtask_serializer = SubtaskSerializer(data=new_subtask_data)
                 if subtask_serializer.is_valid():
                     subtask_serializer.save()
             else:
-                subtask = Subtask.objects.get(id=subtask_data['id'])
-                subtask_serializer = SubtaskSerializer(subtask, data=subtask_data, partial=True)
+                subtask = Subtask.objects.get(id=new_subtask_data['id'])
+                subtask_serializer = SubtaskSerializer(subtask, data=new_subtask_data)
                 if subtask_serializer.is_valid():
                     subtask_serializer.save()
 
