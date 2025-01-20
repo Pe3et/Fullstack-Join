e4ec4eb294b7e6032cf3d388014f8094d3ae2d88
Pe3et <wolfpeeet@googlemail.com>
2025-01-20 16:18:14 +0100

    moved registration serializer to user_auth_app

--- Backend Join/join/api/serializers.py
+++ Backend Join/join/api/serializers.py
@@ -26,27 +26,3 @@ class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
         fields = '__all__'
-
-
-class RegistartionSerializer(serializers.ModelSerializer):
-
-    repeated_password = serializers.CharField(write_only=True)
-
-    class Meta:
-        model = User
-        fields = ['username', 'email', 'password', 'repeated_password']
-        extra_kwargs = {
-            'password': {'write_only': True}
-        }
-
-    def save(self):
-        pw = self.validated_data['password']
-        repeated_pw = self.validated_data['repeated_password']
-
-        if pw != repeated_pw:
-            raise serializers.ValidationError({'error': 'Passwords do not match'})
-        
-        account = User(email=self.validated_data['email'], username=self.validated_data['username'])
-        account.set_password(pw)
-        account.save()
-        return account
