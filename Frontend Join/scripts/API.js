1c753a298af243250d1383ec886e8f7ca0da7121
Pe3et <wolfpeeet@googlemail.com>
2025-01-21 16:07:38 +0100

    added local storage tokens to authorization headers for api requests

--- Frontend Join/scripts/API.js
+++ Frontend Join/scripts/API.js
@@ -6,7 +6,14 @@ const BASE_URL = "http://127.0.0.1:8000/api/";
  * @param {string} [path=""] - The path to the data in the database.
  */
 async function getFromDB(path = "") {
-    const fetchdata = await fetch(BASE_URL + path);
+    const token = getUserTokenFromLocalStorage()
+
+    const fetchdata = await fetch(BASE_URL + path, {
+        method: "GET",
+        headers: {
+            "Authorization": token
+        }
+    });
     const result = await fetchdata.json();
     return result
 }
@@ -18,10 +25,13 @@ async function getFromDB(path = "") {
  * @param {string} [path=""] - The path to the data in the database.
  */
 async function postToDB(postData, path = "") {
+    const token = getUserTokenFromLocalStorage()
+
     const fetchdata = await fetch(BASE_URL + path, {
         method: "POST",
         headers: {
-            "Content-Type": "application/json"
+            "Content-Type": "application/json",
+            "Authorization": token
         },
         body: JSON.stringify(postData)
     });
@@ -36,10 +46,12 @@ async function postToDB(postData, path = "") {
  * @param {string} [path=""] - The path to the data in the database.
  */
 async function putToDB(putData, path = "") {
+    const token = getUserTokenFromLocalStorage()
     await fetch(BASE_URL + path, {
         method: "PUT",
         headers: {
-            "Content-Type": "application/json"
+            "Content-Type": "application/json",
+            "Authorization": token
         },
         body: JSON.stringify(putData)
     });
@@ -51,7 +63,27 @@ async function putToDB(putData, path = "") {
  * @param {string} [path=""] - The path to the data in the database.
  */
 async function deleteFromDB(path = "") {
+    const token = getUserTokenFromLocalStorage()
     await fetch(BASE_URL + path, {
         method: "DELETE",
+        headers: {
+            "Authorization": token
+        }
     })
+}
+
+/**
+ * Tries to get the users token from local storage.
+ * Set's the value for the Authorization header to null, if no token exists in storage.
+ * 
+ * @returns {string} - The Token string for the Authorization header for a request.
+ */
+function getUserTokenFromLocalStorage() {
+    try {
+        token = JSON.parse(localStorage.getItem('joinStorage')).token ?? null
+    } catch (error) {
+        token = null
+    }
+    authorization_header = token ? `Token ${token}` : null
+    return authorization_header
 }
